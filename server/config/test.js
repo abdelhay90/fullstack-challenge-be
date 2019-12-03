6325b7c0d65572e3574e44150148a25ee5ef2b9c
Ahmed Abdelhay <ahmed.abdelhay@andela.com>
2019-12-03 15:08:13 +0200

    updates

--- server/config/test.js
+++ server/config/test.js
@@ -2,7 +2,7 @@ module.exports = {
     // disbable logging for testing
     db: {
         'username': 'root',
-        'password': 'password',
+        'password': null,
         'database': 'database_test',
         'host': '127.0.0.1',
         'dialect': 'mysql',
