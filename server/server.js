d910c199eb6c60b0e0f6651b7961cd50411253ea
Ahmed Abdelhay <ahmed.abdelhay@andela.com>
2019-12-04 02:03:59 +0200

    updates

--- server/server.js
+++ server/server.js
@@ -1,29 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const api = require('./api/api');
-const config = require('./config/config');
-const db = require('./models');
-const errorHandler = require('./middleware/errorHandler');
+const api = require("./api/api");
+const config = require("./config/config");
+const db = require("./models");
+const errorHandler = require("./middleware/errorHandler");
 // const auth = require('./api/auth/routes');
 // db.url is different depending on NODE_ENV
 // todo: db connection initialization
 
 if (config.seed) {
-    // require('./util/seed');
+  // require('./util/seed');
 }
-if (config.env === 'development' && process.argv[2] === '-u') {
-    db.sequelize.sync({ force: true }).then(async () => {});
+if (config.env === "development" && process.argv[2] === "-u") {
+  db.sequelize.sync({ force: true }).then(async () => {
+  });
 }
 // setup the app middleware
-require('./middleware/appMiddleware')(app);
+require("./middleware/appMiddleware")(app);
 
-app.use('/', express.static('build'));
+app.use("/", express.static("build"));
 
 // setup the api
-app.use('/api', api);
+app.use("/api", api);
 
-app.get('/*', (req, res) => {
-    res.sendFile('../build/index.html');
+app.get("/*", (req, res) => {
+  res.sendFile(__dirname + "../build/index.html");
 });
 
 // set up global error handling
